ad46d48bd537179b8b81d207ed57a4e7e8910863
adilveliyev03 <adilveliyev03@gmail.com>
2024-01-15 20:42:04 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+
+    let.splitString = str.split("");
+    console.log(splitString);
+
+    let reverseArray = splitString.reverse();
+    console.log(reverseArray);
+
+
+let joinArray = reverseArray.join("")
+
+    return joinArray;  }
